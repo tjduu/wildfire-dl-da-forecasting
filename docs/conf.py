abf79e9fa0f941fa286e7ca59b930188f3775fa6
Daniel Seal <143702250+edsml-ds423@users.noreply.github.com>
2024-05-24 12:45:38 +0100

    Update conf.py - linting 101

--- docs/conf.py
+++ docs/conf.py
@@ -1,29 +1,30 @@
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../atlaswildfiretool'))
 
-project = 'ACDS3 Wildfire Atlas'
-copyright = '2024, Tim Du, Quan Gu, Jiangnan Meng, Shiyuan Meng, Daniel Seal, Anna Smith, Maddy Wiebe, Xuan Zhan'
-author = 'Tim Du, Quan Gu, Jiangnan Meng, Shiyuan Meng, Daniel Seal, Anna Smith, Maddy Wiebe, Xuan Zhan'
-release = '1.0'
+sys.path.insert(0, os.path.abspath("../atlaswildfiretool"))
+
+project = "ACDS3 Wildfire Atlas"
+copyright = "2024, Tim Du, Quan Gu, Jiangnan Meng, Shiyuan Meng, Daniel Seal, Anna Smith, Maddy Wiebe, Xuan Zhan"
+author = "Tim Du, Quan Gu, Jiangnan Meng, Shiyuan Meng, Daniel Seal, Anna Smith, Maddy Wiebe, Xuan Zhan"
+release = "1.0"
 
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.viewcode',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
 ]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
-language = 'English'
+language = "English"
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "alabaster"
+html_static_path = ["_static"]
 
 # Ensure autodoc includes all members
 autodoc_default_options = {
-    'members': True,
-    'undoc-members': True,
-    'show-inheritance': True,
+    "members": True,
+    "undoc-members": True,
+    "show-inheritance": True,
 }
